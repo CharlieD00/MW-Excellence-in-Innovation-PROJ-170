d4fc0109b0fba9fbd451e125a41d6d77756ecbda
CharlieD00 <108063222+CharlieD00@users.noreply.github.com>
2022-06-23 12:18:07 +0100

    Add files via upload
    Requires training - the Agent saved was too big for GitHub

---
+++ Walk w more DoF in legs/CH_humanoid_walker_rl_parameters.m
@@ -0,0 +1,181 @@
+%% Humanoid Walker With Reinforcement Learning Intialization
+% Copyright 2019 The MathWorks, Inc.
+
+%% Joint rotation limits
+%These limits are similar to human joint rotation limits. 
+
+% Legs joints
+params.jointLimits.hipFrontalUpperLimit  =  30;  % (Deg)
+params.jointLimits.hipFrontalLowerLimit  = -90;  % (Deg)
+params.jointLimits.kneeUpperLimit        =  90;  % (Deg)
+params.jointLimits.kneeLowerLimit        =  5;   % (Deg)
+params.jointLimits.ankleUpperLimit       =  20;  % (Deg)
+params.jointLimits.ankleLowerLimit       = -20;  % (Deg)
+% Arm joints
+params.jointLimits.shoulderFrontalUpperLimit        =  110; % (Deg)
+params.jointLimits.shoulderFrontalLowerLimit        = -30;  % (Deg)
+params.jointLimits.shoulderSagittalUpperLimit       =  90;  % (Deg)
+params.jointLimits.shoulderSagittalLowerLimit       = -30;  % (Deg)
+params.jointLimits.elbowLowerLimit                  = 5;    % (Deg)
+params.jointLimits.elbowUpperLimit                  = 130;  % (Deg)
+params.jointLimits.wristLowerLimit                  = -90;  % (Deg)
+params.jointLimits.wristUpperLimit                  = 70;   % (Deg)
+
+%% Motor Parameters: Left
+% Current motor: https://uk.rs-online.com/web/p/dc-motors/7736804
+%Stall Torque
+params.motor.StallTorque.LeftHip                        = 100; % (Nm)
+params.motor.StallTorque.LeftKnee                       = 100; % (Nm)
+params.motor.StallTorque.LeftAnkle                      = 100; % (Nm)
+
+params.motor.StallTorque.LeftShoulderFrontal            = 100; % (Nm)
+params.motor.StallTorque.LeftShoulderSagital            = 100; % (Nm)
+params.motor.StallTorque.LeftElbow                      = 100; % (Nm)
+params.motor.StallTorque.LeftWrist                      = 100; % (Nm)
+
+%Armature Inductance: UNKNOWN
+params.motor.ArmatureInductance.LeftHip                 = 12e-6;
+params.motor.ArmatureInductance.LeftKnee                = 12e-6;
+params.motor.ArmatureInductance.LeftAnkle               = 12e-6;
+
+params.motor.ArmatureInductance.LeftShoulderFrontal     = 12e-6;
+params.motor.ArmatureInductance.LeftShoulderSagital     = 12e-6;
+params.motor.ArmatureInductance.LeftElbow               = 12e-6;
+params.motor.ArmatureInductance.LeftWrist               = 12e-6;
+
+%No-Load Speed
+params.motor.NoLoadSpeed.LeftHip                        = 22.5; % (RPM)
+params.motor.NoLoadSpeed.LeftKnee                       = 22.5; % (RPM)
+params.motor.NoLoadSpeed.LeftAnkle                      = 22.5; % (RPM)
+
+params.motor.NoLoadSpeed.LeftShoulderFrontal            = 22.5; % (RPM)
+params.motor.NoLoadSpeed.LeftShoulderSagital            = 22.5; % (RPM)
+params.motor.NoLoadSpeed.LeftElbow                      = 22.5; % (RPM)
+params.motor.NoLoadSpeed.LeftWrist                      = 22.5; % (RPM)
+
+%Torque Constant
+params.motor.RatedDCSupply.LeftHip                      = 12; % (V)
+params.motor.RatedDCSupply.LeftKnee                     = 12; % (V)
+params.motor.RatedDCSupply.LeftAnkle                    = 12; % (V)
+
+params.motor.RatedDCSupply.LeftShoulderFrontal          = 12; % (V)
+params.motor.RatedDCSupply.LeftShoulderSagital          = 12; % (V)
+params.motor.RatedDCSupply.LeftElbow                    = 12; % (V)
+params.motor.RatedDCSupply.LeftWrist                    = 12; % (V)
+
+%% Motor Parameters: Right
+%Stall Torque
+params.motor.StallTorque.RightHip                        = 100; % (Nm)
+params.motor.StallTorque.RightKnee                       = 100; % (Nm)
+params.motor.StallTorque.RightAnkle                      = 100; % (Nm)
+
+params.motor.StallTorque.RightShoulderFrontal            = 100; % (Nm)
+params.motor.StallTorque.RightShoulderSagital            = 100; % (Nm)
+params.motor.StallTorque.RightElbow                      = 100; % (Nm)
+params.motor.StallTorque.RightWrist                      = 100; % (Nm)
+
+%Armature Inductance
+params.motor.ArmatureInductance.RightHip                 = 12e-6;
+params.motor.ArmatureInductance.RightKnee                = 12e-6;
+params.motor.ArmatureInductance.RightAnkle               = 12e-6;
+
+params.motor.ArmatureInductance.RightShoulderFrontal     = 12e-6;
+params.motor.ArmatureInductance.RightShoulderSagital     = 12e-6;
+params.motor.ArmatureInductance.RightElbow               = 12e-6;
+params.motor.ArmatureInductance.RightWrist               = 12e-6;
+
+%No-Load Speed
+params.motor.NoLoadSpeed.RightHip                        = 22.5; % (RPM)
+params.motor.NoLoadSpeed.RightKnee                       = 22.5; % (RPM)
+params.motor.NoLoadSpeed.RightAnkle                      = 22.5; % (RPM)
+
+params.motor.NoLoadSpeed.RightShoulderFrontal            = 22.5; % (RPM)
+params.motor.NoLoadSpeed.RightShoulderSagital            = 22.5; % (RPM)
+params.motor.NoLoadSpeed.RightElbow                      = 22.5; % (RPM)
+params.motor.NoLoadSpeed.RightWrist                      = 22.5; % (RPM)
+
+%Torque Constant
+params.motor.RatedDCSupply.RightHip                      = 12; % (V)
+params.motor.RatedDCSupply.RightKnee                     = 12; % (V)
+params.motor.RatedDCSupply.RightAnkle                    = 12; % (V)
+
+params.motor.RatedDCSupply.RightShoulderFrontal          = 12; % (V)
+params.motor.RatedDCSupply.RightShoulderSagital          = 12; % (V)
+params.motor.RatedDCSupply.RightElbow                    = 12; % (V)
+params.motor.RatedDCSupply.RightWrist                    = 12; % (V)
+
+
+%% Material properties
+
+params.materialProperties.lowerBodyDensity = 990;  % (kg/m^3)
+params.materialProperties.upperBodyDensity = 1900; % (kg/m^3)
+
+%% World damping
+% We add damping to the vertical (z) and rotational (Rx, Ry, Rz) components
+% of the bushing joint. This dissipates energy from the system and facilates 
+% quicker learning. Increasing this improves stability and learning, but is
+% less realistic. 
+
+params.simulation.worldDamping = 6; % (Ns/m)
+
+%% Simulation parameters
+
+params.simulation.initialHeight=1.54; % (m) Intial height of robot
+params.simulation.Ts = 0.025;         % (s) Control and sensing discretization time
+params.simulation.Tf = 600;      % (s) Max simulation time
+
+%% Spatial contact force block parameters. 
+% The value of these parameters affects simulation and learning speed. 
+% Higher damping generally improves learning but slows down simulation. 
+% High static and dynamic friction values tend to improve learning. 
+
+params.contact.stiffness        = 1e5;  % (N/m)
+params.contact.damping          = 1e5;  % (Ns/m) 
+params.contact.transitionWidth  = 1e-3; % (m)
+params.contact.staticFriction   = 1;    % () 
+params.contact.dynamicFriction  = 0.9;  % ()
+params.contact.criticalVelocity = 1e-3; % (m/s) 
+params.contact.contactRadius = 0.01;    % (m)
+
+%% Controller parameters
+
+params.controller.hipFrontalStiffness  = 80; % (N*m/rad)
+params.controller.hipFrontalDamping    = 1;  % (N*m*s/rad)
+params.controller.kneeStiffness        = 80; % (N*m*/rad)
+params.controller.kneeDamping          = 1;  % (N*m*s/rad)
+params.controller.ankleStiffness       = 80; % (N*m/rad)
+params.controller.ankleDamping         = 1;  % (N*m*s/rad)
+   
+
+%% Stopping criteria
+% Define the conditions under which the simulation is terminated early. 
+% One or more of three conditions need to be satisfied for termination. 
+% If the humanoid torso drops vertically, travels laterally or rotates in
+% any axis more than a set of predefined values, or if the humanoid stops
+% moving.
+
+params.stoppingCriteria.heightChange     = 0.5; % (m)
+params.stoppingCriteria.lateral          = 1;   % (m)
+params.stoppingCriteria.angle            = 45;  % (deg)
+params.stoppingCriteria.timeoutTime      = 2;   % (s)
+params.stoppingCriteria.timeoutDistance  = 1;   % (m)
+
+%% Reward scaling parameters
+
+params.reward.forwardRewardWeight   = 2;    % Forward velocity scale, w_1
+params.reward.timestepRewardWeight  = 1;    % Not falling scale, w_2
+params.reward.powerPenaltyWeight    = 5e-4; % Power scale, w_3
+params.reward.verticalPenaltyWeight = 25;   % Vertical displacement scale, w_4
+params.reward.lateralPenaltyWeight  = 10;  % Lateral displacement scale, w_5
+
+%% Display parameters 
+
+params.display.tileColour    = [0.956, 0.941, 0.941];
+params.display.floorColour   = [0.756, 0.741, 0.741];
+params.display.tileThickness = 0.005; 
+params.display.planeWidth        = 2;   % (m)
+params.display.planeLength       = 50;  % (m)
+params.display.planeHeight       = 1;   % (m)
+params.display.Gravity           = [0, 0, -9.80665];   % (m/s^2)
+
+
